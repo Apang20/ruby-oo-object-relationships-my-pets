8b6407361b6e67a5115f564e3825e1014276bcd9
github username <github email address>
2020-11-19 20:18:08 -0600

    PetsLab

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
-  # code goes here
+  attr_accessor :mood, :owner 
+  attr_reader :name
+ 
+  @@all = [] #holds all the info of this class 
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+
+    self.class.all << self  #shoves the current dog to []
+  end
+
+  def self.all  #allows you to use the info of this class 
+    @@all
+  end 
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+attr_accessor :mood, :owner
+attr_reader :name
+
+@@all = [] #holds all the info of this class 
+
+def initialize(name, owner)
+  @name = name
+  @owner = owner 
+  @mood = "nervous"
+
+  self.class.all << self #pushes the dog info to the []
+end
+
+def self.all #allows you to use the dog []
+  @@all
+end 
 end
